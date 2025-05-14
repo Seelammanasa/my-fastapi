00336fe73ad9edbfcd7c3ef63d3619af48688a78
seelammanasa <seelammanasa99@gmail.com>
2025-05-14 21:42:00 +0530

    db new ip address

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://manasa:manasa@16.170.172.9/eat_pizza"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://manasa:manasa@13.48.105.78/eat_pizza"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,bind=engine)
