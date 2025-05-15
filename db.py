7c7d10a087533c4eb4bb735fcc27ddff94eac90c
seelammanasa <seelammanasa99@gmail.com>
2025-05-15 14:33:55 +0530

    db update

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://manasa:manasa@13.48.105.78/eat_pizza"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://manasa:manasa@13.49.241.233/eat_pizza"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,bind=engine)
