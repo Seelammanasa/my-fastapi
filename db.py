e553bcc3a73a6713ffa912c93fd6a428ff61dbe6
seelammanasa <seelammanasa99@gmail.com>
2025-05-21 19:36:37 +0530

    db update

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,declarative_base
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://manasa:manasa@13.49.241.233/eat_pizza"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_pre_ping=True,    
-    pool_recycle=1800      
+    pool_recycle=1800,
+    pool_size=5,              
+    max_overflow=2     
 )
 SessionLocal=sessionmaker(autocommit=False,bind=engine)
+Base = declarative_base()
